1b9c1878341ba4d436635d12577f5bf891f324cf
ejMina226 <118474890+ejMina226@users.noreply.github.com>
2023-11-16 10:09:57 +0000

    Remove old test

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -5,20 +5,6 @@ import pandas as pd
 from uptime_service_validation.coordinator.helper import (filterStateHashPercentage, createGraph, applyWeights, bfs)
 import pandas as pd
 
-# The folloiwng two tests will fail as I have not given an accurate bucket name.
-# def testFilePullSmallRange(self):
-#     start_time = datetime.strptime('2023-08-03T16:31:58Z',"%Y-%m-%dT%H:%M:%SZ")
-#     end_time = datetime.strptime('2023-08-03T16:31:59Z', "%Y-%m-%dT%H:%M:%SZ")
-#     filtered_list = pullFileNames(start_time, end_time, "block-bucket-name", True)
-#     self.assertEqual(len(filtered_list), 1)
-
-# def testFilePullLargeRange(self):
-#     start_time = datetime.strptime('2023-08-03T16:32:00Z',"%Y-%m-%dT%H:%M:%SZ")
-#     end_time = datetime.strptime('2023-08-03T16:33:59Z', "%Y-%m-%dT%H:%M:%SZ")
-#     filtered_list = pullFileNames(start_time, end_time, "block-bucket-name", True)
-#     self.assertEqual(len(filtered_list), 11)
-
-
 def test_get_time_batches():
     a = datetime(2023, 11, 6, 15, 35, 47, 630499)
     b = a + timedelta(minutes=5)
