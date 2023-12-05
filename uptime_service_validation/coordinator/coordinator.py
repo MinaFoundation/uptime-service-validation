d476cc67cbf38f8796560faa73bfcc5db5776eed
ejMina226 <118474890+ejMina226@users.noreply.github.com>
2023-12-05 14:29:09 +0000

    Remove line

--- uptime_service_validation/coordinator/coordinator.py
+++ uptime_service_validation/coordinator/coordinator.py
@@ -100,13 +100,14 @@ def main():
                     prev_batch_end, cur_batch_end
                 )
             )
-            test = os.environ.get("WEBHOOK_URL")
-            webhookURL = os.environ.get("WEBHOOK_URL")
-            if (end - start < float(os.environ["ALARM_ZK_LOWER_LIMIT_SEC"])): 
-                sendSlackMessage(webhookURL, f'ZkApp Validation took {end- start} seconds, which is too quick', logging)
-            if (end - start > float(os.environ["ALARM_ZK_UPPER_LIMIT_SEC"])): 
-                sendSlackMessage(webhookURL, f'ZkApp Validation took {end- start} seconds, which is too long', logging)
 
+            webhookURL = os.environ.get("WEBHOOK_URL")
+            if (webhookURL != None):
+                if (end - start < float(os.environ["ALARM_ZK_LOWER_LIMIT_SEC"])): 
+                    sendSlackMessage(webhookURL, f'ZkApp Validation took {end- start} seconds, which is too quick', logging)
+                if (end - start > float(os.environ["ALARM_ZK_UPPER_LIMIT_SEC"])): 
+                    sendSlackMessage(webhookURL, f'ZkApp Validation took {end- start} seconds, which is too long', logging)
+                    
             submissions = []
             cassandra = AWSKeyspacesClient()
             try:
